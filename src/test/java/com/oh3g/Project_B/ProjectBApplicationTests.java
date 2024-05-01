13b1ac22f41f31f15d2119be455440548567561e
Hyoungtaek <aqko0719@gmail.com>
2024-05-01 22:39:43 +0900

    Initial commit

---
+++ src/test/java/com/oh3g/Project_B/ProjectBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oh3g.Project_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
